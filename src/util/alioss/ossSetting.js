3cd1127cc02c058ba4981434ac3c89a7d482735d
gaochaowyq <76209609@qq.com>
2023-06-23 16:34:21 +0800

    Update ossSetting.js

--- src/util/alioss/ossSetting.js
+++ src/util/alioss/ossSetting.js
@@ -1,6 +1,6 @@
 const ossregion = "oss-cn-beijing"
 const accessKeyId = "LTAI5tKm8k72fUPBSApJqcBy"
-const accessKeySecret = "aQMMUyKmXbywTnYJaFExDDNr4jH398"
+const accessKeySecret = ""
 const bucketName = "bat-mytry"
 
 export {ossregion,accessKeySecret,accessKeyId,bucketName}
